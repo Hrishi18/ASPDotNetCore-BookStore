42cf1a1792e5d3d0c9441ca87c2b1f32b5cae6e8
Hrishikesh Gawas <hrishikesh.gawas99@gmail.com>
2021-09-23 15:47:24 +0530

    Added libman to use client side library

--- BookStore/BookStore/Controllers/HomeController.cs
+++ BookStore/BookStore/Controllers/HomeController.cs
@@ -8,9 +8,21 @@ namespace BookStore.Controllers
 {
     public class HomeController : Controller
     {
-        public string Index()
+        public ViewResult Index()
         {
-            return "Hrishikesh";
+            return View();
         }
+
+        public ViewResult AboutUs()
+        {
+            return View();
+        }
+
+        public ViewResult ContactUs()
+        {
+            return View();
+        }
+
+
     }
 }
---
+++ BookStore/BookStore/Repository/BookRepository.cs
@@ -0,0 +1,39 @@
+﻿using BookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Repository
+{
+    public class BookRepository
+    {
+        public List<BookModel> GetAllBooks()
+        {
+            return DataSource();
+        }
+
+        public BookModel GetBookById(int id)
+        {
+            return DataSource().Where(x => x.Id == id).FirstOrDefault();
+        }
+
+        public List<BookModel> SearchBook(string title, string authorName)
+        {
+            return DataSource().Where(x => x.Title.Contains(title) && x.Author.Contains(authorName)).ToList();
+        }
+
+        public List<BookModel> DataSource()
+        {
+            return new List<BookModel>()
+            {
+                new BookModel(){Id=1, Title="C", Author="James" },
+                new BookModel(){Id=2, Title="Java", Author="John" },
+                new BookModel(){Id=3, Title="Python", Author="Adam" },
+                new BookModel(){Id=4, Title="Dart", Author="Kate" },
+                new BookModel(){Id=5, Title="Swift", Author="Harry" },
+
+            };
+        }
+    }
+}
